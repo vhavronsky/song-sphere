6f954a29df3a0db7c4daf3dc0b8205ec9a22856c
Vasyl Havronskyi <vaha@opinov8.com>
2023-02-25 12:12:17 +0200

    feat: health endpoint

---
+++ api/src/application/controllers/health.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+
+@Controller('health')
+export class HealthController {
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  checkHealth(): string {
+    return 'OK';
+  }
+}
