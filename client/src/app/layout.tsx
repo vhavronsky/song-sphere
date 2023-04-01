bae5ac3847a4f399555cc566c75edc962441ec04
Vasyl Havronskyi <vaha@opinov8.com>
2023-04-01 18:49:13 +0300

    feat: add .nvmrc and .prettierrc

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,18 +1,16 @@
-import { FC, PropsWithChildren } from "react"
+import { FC, PropsWithChildren } from 'react';
 
 export const metadata = {
-    title: 'Example title',
-    description: 'Example description',
-}
+  title: 'Example title',
+  description: 'Example description',
+};
 
 const RootLayout: FC<PropsWithChildren> = ({ children }) => {
-    return (
-        <html lang="en">
-            <body>
-                {children}
-            </body>
-        </html>
-    )
-}
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+};
 
-export default RootLayout
+export default RootLayout;
