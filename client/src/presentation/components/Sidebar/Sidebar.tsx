505211c7753b0507ca75c19640971710eeb3812b
Vasyl Havronskyi <vaha@opinov8.com>
2023-05-28 12:09:39 +0300

    refactor: rename SideBar to Sidebar

--- client/src/presentation/components/Sidebar/Sidebar.tsx
+++ client/src/presentation/components/Sidebar/Sidebar.tsx
@@ -1,14 +1,14 @@
 import { FC } from 'react';
 import Link from 'next/link';
 
-export const SideBar: FC = () => {
+export const Sidebar: FC = () => {
   return (
     <aside className="sidebar">
       <Link className="link" href="/">
         Home
       </Link>
       <br />
-      SideBar
+      Sidebar
     </aside>
   );
 };
